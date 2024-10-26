b58ed3a1c789e46e1fadf65a02164757c1ea6cdd
ethun <ethuntcg@gmail.com>
2024-10-26 22:40:48 +0600

    update

---
+++ script.js
@@ -0,0 +1,12 @@
+function audioPlay(){
+     const audio= new Audio('bubble Pop.mp3');
+     audio.play();
+}
+function audio() {
+     const audio = new Audio('Download.mp3');
+     audio.play();
+ 
+     setTimeout(() => {
+         window.location.href = "https://drive.google.com/file/d/14-QZ_OKW1kyTwQTmxjEDCVV6UnsANMKt/view?usp=drivesdk'";
+     }, 3000);
+ }
